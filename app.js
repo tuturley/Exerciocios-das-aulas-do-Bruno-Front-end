bc2925dd9e0fb91e460e813f54b262504281c72c
tuturley <tuturley@gmail.com>
2025-05-13 22:20:02 -0300

    resolvendo conflito após pull, e começando com arrays

--- app.js
+++ app.js
@@ -1,5 +1,2 @@
-let numero = prompt("Digite um numero de 1 a 10");
-
-for(i=0; i<=10; i++){
-    console.log(`O numero ${numero} x ${i} é igual a ${numero8*i}`)
-}
+let alunos = ["Arthur", 20, "Fiap", "Meus ovos", true]
+console.log(alunos[3])
